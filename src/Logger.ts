f38850c8a6aafb6470d23ee8977938f7de260cde
Carlos Alvarez <carlosxviii@gmail.com>
2018-11-22 14:48:45 -0300

    added logger colors

--- src/Logger.ts
+++ src/Logger.ts
@@ -1,42 +1,38 @@
 import * as path from 'path'
 import * as fs from 'fs'
+import * as appRoot from 'app-root-path'
+import chalk from 'chalk'
 
+/*
+ *
+ * @class Logger
+ * */
 export default class Logger {
 
   /*
-   *
-   * @var Array the type if log
-   *
+   * @var {Array} the type if log
    * */
   private static logType : object = { 1 : 'ERROR', 2 : 'WARNING', 3: 'LOG'} 
 
   /*
-   *
-   * @var boolean file need to be created?
-   *
+   * @var {boolean} file need to be created?
    * */
-  private static mustLogToFile : boolean = false;
+  private static mustLogToFile : boolean = true;
   /*
-   *
-   * @var string the path and filename to save the log
-   *
+   * @var {string} the path and filename to save the log
    * */
-  private static logPathAndFile : string = '../storage/bearded.log';
+  private static logPathAndFile : string = '/storage/bearded.log';
 
   /*
-   *
-   * @var string the root path of the app
-   *
+   * @var {string} the root path of the app
    * */
-  private static appDir : string = path.dirname(require.main.filename);
+  private static appDir : string;
  
   /*
    *
    * The log of warning
-   *
-   * @param string message
-   *
-   * @return void
+   * @param {string} message
+   * @return {void}
    * */
   public static warning (...args) : void{
 
@@ -48,16 +44,13 @@ export default class Logger {
       return
     }
     const msg = Logger.msgFormatter(2, args[0]);
-    Logger.logToFile(msg);
+    Logger.logToFile(chalk.yellow(msg));
   }
 
   /*
-   *
    * The log of info
-   *
-   * @param string message
-   *
-   * @return void
+   * @param {string} message
+   * @return {void}
    * */
   public static info (...args) : void{
 
@@ -73,41 +66,30 @@ export default class Logger {
   } 
 
   /*
-   *
    * The log of count
-   *
-   * @param string lable
-   *
-   * @return void
+   * @param {string} lable
+   * @return {void}
    * */
   public static count (label : string) : void{
 
     console.count(label);
-  
   }
   
   /*
-   *
    * The log lod assert
-   *
-   * @param string message
-   *
-   * @return void
+   * @param {string} message
+   * @return {{void}
    * */
   public static assert (message : string) : void{
   
     console.assert(false, "You have no element with ID 'demo'");
-  
   }
 
   /*
-   *
    * The log of error
-   *
-   * @param string message
-   * @param error err
-   *
-   * @return void
+   * @param {string} message
+   * @param {error} err
+   * @return {void}
    * */
   public static error (...args) : void{
 
@@ -127,32 +109,27 @@ export default class Logger {
     const err = args['err'];
     if(err != null){
       console.error(msg.slice(0, -2), err);
-      Logger.logToFile(msg, err);
+      Logger.logToFile(chalk.red(msg), chalk.red(err));
       return;
     }
     console.error(msg.slice(0, -2));
-    Logger.logToFile(msg);
+    Logger.logToFile(chalk.red(msg));
     console.trace();
   }
 
   /*
    *
    * The log on file 
-   *
-   * @param string message
-   * @param error errTOp
-   *
-   * @return void
+   * @param {string} message
+   * @param {error} errTOp
+   * @return {void}
    * */
   private static logToFile(msg : string,   errTop = null) : void {
   
     if(!this.mustLogToFile)
       return;
 
-    /*if(errTop)
-      msg += `${errTop.name} \n`;*/
-
-    fs.appendFile(`${Logger.appDir}/${Logger.logPathAndFile}`, msg, function(err) {
+    fs.appendFile(`${appRoot}/${Logger.logPathAndFile}`, msg, function(err) {
       
     if(err) 
       console.error(Logger.logType[1], 'There was an error writing the file', err);
@@ -161,13 +138,10 @@ export default class Logger {
   }
 
   /*
-   *
    * message formatter
-   *
-   * @param int logType
-   * @param string message
-   *
-   * @return void
+   * @param {int} logType
+   * @param {string} message
+   * @return {void}
    * */
   private static msgFormatter(logType : number , message : string){
     
@@ -178,14 +152,11 @@ export default class Logger {
   }
 
   /*
-   *
    * name of the current file
-   *
-   * @return string
+   * @return {string}
    * */
   private static fileName() : string{
   
     return path.basename(__filename);
   }
-
 }
